eee1a224b86af7d315e5f19f783842e76bee6a37
David Anthony Bau <abau@mit.edu>
2017-05-30 16:06:02 -0400

    Finish comparison script

--- compare.lua
+++ compare.lua
@@ -1,5 +1,6 @@
 -- Compute several statistics given two encoding distribution samples.
 -- Currently computes:
+--  - Covariance
 --  - Correlation
 
 function main()
@@ -11,23 +12,41 @@ function main()
   local A, B = torch.load(opt.A), torch.load(opt.B)
   local encodings_A, encodings_B = A['encodings'], B['encodings']
   local mean_A, mean_B = A['mean'], B['mean']
+  local sigma_A, sigma_B = A['stdev'], B['stdev']
   local sample_length = A['sample_length'] -- should also == B['sample_length']
 
-  local E_AB = 
+  -- Measure the rnn size by looking at the size of an
+  -- encoding
+  local rnn_size_A = encodings_A[1]:size()[2]
+  local rnn_size_B = encodings_B[1]:size()[2]
+
+  -- Initialize
+  local E_AB = torch.Tensor(rnn_size_A, rnn_size_B):zero():cuda()
 
   -- For each line in the input sample,
   for i=1,#encodings_A do
     -- For each token in this line
-    vectors_A, vectors_B = encodings_A[i], encodings_B[i]
+    vectors_A, vectors_B = encodings_A[i]:cuda(), encodings_B[i]:cuda() -- 1 x rnn_size
     for j=1,#vectors_A do
-      E_AB:add(1 / sample_length, )
+      -- rnn_size x 1 X 1 x rnn_size = rnn_size x rnn_size
+      -- Take the product and add it to our average
+      E_AB:add(1 / sample_length, torch.mm(vectors_A[j]:transpose(1, 2), vectors_B[j]))
     end
-
-    -- Take the product and add it to our average
   end
 
+  -- Also take the product of the means
+  local EA_EB = torch.mm(mean_A:transpose(1, 2), mean_B)
+
+  local sigma = torch.mm(sigma_A:transpose(1, 2), sigma_B)
+
+  local covariance = torch.csub(E_AB, EA_EB)
+  local correlation = corch.cdiv(covariance, sigma)
+
   -- Save the encodings
-  torch.save(encodings, out_file)
+  torch.save({
+    ['correlation'] = correlation,
+    ['covariance'] = covariance
+  }, opt.out_file)
 end
 
 main()
--- describe.lua
+++ describe.lua
@@ -19,22 +19,33 @@ function main()
   -- Encode each line in the input sample file
   for line in file:lines() do
     encoding = beam.encode(line)
-    table.insert(encodings, encoding[1]) -- encoding[1] should be size_l x rnn_size
+    table.insert(encodings, encoding[1:cuda()]) -- encoding[1] should be size_l x rnn_size
     total_token_length = total_token_length + encoding:size()[2]
   end
 
   -- Get the average
-  mean = torch.Tensor(encodings[1]:size()[3]):zero()
+  mean = torch.Tensor(encodings[1]:size()[2]):zero():cuda()
   for encoding in encodings do
     mean:add(1 / total_token_length, torch.cumsum(encoding))
   end
+
+  -- Get the stdev
+  stdev = torch.Tensor(encodings[1]:size()[2]):zero():cuda()
+  for encoding in encodings do
+    for i=1,encoding:size()[2] do
+      stdev:add(1 / total_token_length, torch.pow(torch.add(encoding[i], -1 mean), 2))
+    end
+  end
+
+  stdev:sqrt()
 
   -- Save the encodings
   torch.save({
     ['encodings'] = encodings,
     ['mean'] = mean,
+    ['stdev'] = stdev,
     ['sample_length'] = total_token_length
-  }, out_file)
+  }, opt.out_file)
 end
 
 main()
